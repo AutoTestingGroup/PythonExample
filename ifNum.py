6ca1726c5f91d2cf376b23084a592fda38e5c210
legend818 <legend919@163.com>
2019-05-10 16:13:30 +0800

    new

--- ifNum.py
+++ ifNum.py
@@ -13,13 +13,10 @@ while i < 100:
 #判断随机数字是正数、0、负数
     if t > 0:
         L1.append(t)
-
     elif t == 0:
         L2.append(t)
-
     else:
         L3.append(t)
-
     i += 1
 print('产生了如下正数：',L1,'次数为：',len(L1))
 print('产生了如下0：',L2,'次数为：',len(L2))

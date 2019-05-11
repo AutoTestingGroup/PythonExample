1aa568006fbe8a7dfc875a31341a92605a28c59d
legend818 <legend919@163.com>
2019-05-11 12:49:15 +0800

    commit

--- maxValue.py
+++ maxValue.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Filename maxValue.py Python 获取最大值函数
 
-def maxvalue():
+def maxvalue(x):
     L = []
     i = 0
-    for i in range(100):
+    for i in range(x):
         L.append(i)
     print(max(L))
-maxvalue()
+maxvalue(3)

3a2a804e4f8cdd9021d9dea2de52b6f7b423492a
legend818 <legend919@163.com>
2019-05-16 18:47:49 +0800

    commit

---
+++ printDict.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+d = {'a': 1, 'b': 2, 'c': 3}
+
+# 打印 key
+for i in d:
+    print(i)
+
+#打印 value
+for i in d.values():
+    print(i)
+# 打印key以及value
+for i in d.items():
+    print(i)
+# 打印key，value
+for i,j in d.items():
+    print(i,j)
+
